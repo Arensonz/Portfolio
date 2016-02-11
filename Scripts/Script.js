7f54028de3f5a724b9ef9e9d292bd760939639ff
Zach Arenson <Zach Arenson>
2016-02-11 15:10:35 -0800

    fade to indicators and some bckgnd sizing

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,18 +1,29 @@
 
 alert("Hello! I am an alert box!!");
 
-alert("Hello! AGAIN!");
-
 $(document).ready(function() {
-    $("#logo").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#intro").offset().top
-        }, 1000);
-    });
-  
-    $(".navbtn").click(function() {
-        $('html, body').animate({
-            scrollTop: $(".navbar").offset().top
-        }, 1000);
-    });
+	
+	$(".carousel-indicators").hide();
+	
+  $("#logo").click(function() {
+    $('html, body').animate({
+      scrollTop: $("#intro").offset().top
+    }, 750);
+  });
+
+  $(".navbtn").click(function() {
+    $('html, body').animate({
+      scrollTop: $(".navbar").offset().top
+    }, 750);
+  });
+	
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 200) {
+      $(".carousel-indicators").fadeIn(200);
+    } else {
+      $(".carousel-indicators").fadeOut(200);
+    }
+	});
+	
 });
