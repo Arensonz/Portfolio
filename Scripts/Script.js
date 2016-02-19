6c7f196d7697c7ed35eb55d322504c1d5dff96c2
Zach Arenson <Zach Arenson>
2016-02-18 16:25:45 -0800

    btn active switching added

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -7,15 +7,37 @@ $(document).ready(function() {
 	
   $("#logo").click(function() {
     $('html, body').animate({
-      scrollTop: $("#intro").offset().top
+      scrollTop: $(".intro, .header-spacer").offset().top
     }, 750);
   });
 
-  $(".navbtn").click(function() {
+  $(".btn").click(function() {
     $('html, body').animate({
       scrollTop: $(".navbar").offset().top
     }, 750);
   });
+	
+	$('.navbar-header button').click(function() {
+
+    $('.navbar-header button.active').removeClass('active');
+    $(this).addClass('active');
+		
+	});
+	
+/*	$('.carousel-indicators:nth-child(n+1):nth-child(-n+5)').hasClass('active')function() {
+		$('.navbar-header button.active').removeClass('active');
+		$('#work').addClass('active');
+	}
+	
+	$('.carousel-indicators:nth-child(6)').hasClass('active') {
+		$('.navbar-header button.active').removeClass('active');
+		$('#about').addClass('active');
+	}
+	
+	$('.carousel-indicators:nth-child(7)').hasClass('active') {
+		$('.navbar-header button.active').removeClass('active');
+		$('#contact').addClass('active');
+	}*/
 	
   $(document).scroll(function() {
     var y = $(this).scrollTop();
