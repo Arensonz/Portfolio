7e5eaf644ebcc6884765d5f2068deceb514fa8ce
Zach Arenson <Zach Arenson>
2016-02-19 17:17:15 -0800

    added activeNav function for btns

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -5,39 +5,77 @@ $(document).ready(function() {
 	
 	$(".carousel-indicators").hide();
 	
+//	Logo Scroll to Intro
   $("#logo").click(function() {
     $('html, body').animate({
       scrollTop: $(".intro, .header-spacer").offset().top
     }, 750);
   });
 
+//	Nav Scroll to Content
   $(".btn").click(function() {
     $('html, body').animate({
       scrollTop: $(".navbar").offset().top
     }, 750);
   });
-	
-	$('.navbar-header button').click(function() {
 
-    $('.navbar-header button.active').removeClass('active');
-    $(this).addClass('active');
-		
+	var activeNav = function () {
+		$('.carousel-indicators li').each(function() {
+			if( $(this).hasClass('active') && $(this).hasClass('work-btn') ) {
+				
+				$('.navbar-header button.active').removeClass('active');
+				$('#work').addClass('active');
+				console.log("work");
+				return false;
+			}
+
+			else if( $(this).hasClass('active') && $(this).hasClass('about-btn') ) {
+				
+				$('.navbar-header button.active').removeClass('active');
+				$('#about').addClass('active');
+				console.log("about");
+			}
+
+			else if( $(this).hasClass('active') && $(this).hasClass('contact-btn') ) {
+				
+				$('.navbar-header button.active').removeClass('active');
+				$('#contact').addClass('active');
+				console.log("contact");
+				return false;
+			}
+			
+			else{
+				
+			}
+		});
+	}
+	$('.navbar-header button').click(function() {
+		activeNav();
 	});
 	
-/*	$('.carousel-indicators:nth-child(n+1):nth-child(-n+5)').hasClass('active')function() {
-		$('.navbar-header button.active').removeClass('active');
-		$('#work').addClass('active');
-	}
+//	Nav Buttons Add/Remove Active Class
+//	$('.navbar-header button').click(function() {
+//
+//    $('.navbar-header button.active').removeClass('active');
+//    $(this).addClass('active');
+//
+//	});
+//	
+	//	Indicators Add/Remove Active Class
+	$('.carousel-indicators li').click(function() {
+		activeNav();
+	});
 	
-	$('.carousel-indicators:nth-child(6)').hasClass('active') {
-		$('.navbar-header button.active').removeClass('active');
-		$('#about').addClass('active');
-	}
+		//	Control Left Add/Remove Active Class
+	$('.left .carousel-control').click(function() {
+		activeNav();
+	});
 	
-	$('.carousel-indicators:nth-child(7)').hasClass('active') {
-		$('.navbar-header button.active').removeClass('active');
-		$('#contact').addClass('active');
-	}*/
+		//	Control Right Add/Remove Active Class
+	$('.right .carousel-control').click(function() {
+		activeNav();
+	});
+	
 	
   $(document).scroll(function() {
     var y = $(this).scrollTop();
