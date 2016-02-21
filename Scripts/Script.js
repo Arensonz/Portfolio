08b0ff84ee79e1e4f6bba346d5860bc573b46b1f
Zach Arenson <Zach Arenson>
2016-02-20 22:43:02 -0800

    made buttons acceptable

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -5,6 +5,50 @@ $(document).ready(function() {
 	
 	$(".carousel-indicators").hide();
 	
+//	Fading Indicators
+	$(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 600) {
+      $(".carousel-indicators").fadeIn(200);
+    } 
+		else {
+      $(".carousel-indicators").fadeOut(200);
+    }
+	});
+	
+//	Remove Active Class When Scrolling Up
+	$(document).scroll(function() {
+		var y = $(this).scrollTop();
+    if (y < 200) {
+      $('.navbar-header button.active').removeClass('active');
+			console.log("remove active class")
+    } 
+		else if (y > 200) {
+			if( $('.work-panel').hasClass('active') ) {
+				$('#about-btn').removeClass('active');
+				$('#contact-btn').removeClass('active');
+				$('#work-btn').addClass('active');
+				console.log("work");
+			}
+			else if( $('.about-panel').hasClass('active') ) {
+				$('#work-btn').removeClass('active');
+				$('#contact-btn').removeClass('active');
+				$('#about-btn').addClass('active');
+				console.log("about");
+			}
+			else if( $('.contact-panel').hasClass('active') ) {
+				$('#work-btn').removeClass('active');
+				$('#about-btn').removeClass('active');
+				$('#contact-btn').addClass('active');
+				console.log("contact");
+			}
+			else{
+			}
+		}
+		else {
+		}
+	});
+	
 //	Logo Scroll to Intro
   $("#logo").click(function() {
     $('html, body').animate({
@@ -15,75 +59,43 @@ $(document).ready(function() {
 //	Nav Scroll to Content
   $(".btn").click(function() {
     $('html, body').animate({
-      scrollTop: $(".navbar").offset().top
+      scrollTop: $(".caroNav").offset().top
     }, 750);
   });
 
-	var activeNav = function () {
+	$('#myCarousel').on('slid.bs.carousel', function (e) {
+		console.log("true");
+
 		$('.carousel-indicators li').each(function() {
 			if( $(this).hasClass('active') && $(this).hasClass('work-btn') ) {
-				
+
 				$('.navbar-header button.active').removeClass('active');
-				$('#work').addClass('active');
+				$('#work-btn').addClass('active');
 				console.log("work");
 				return false;
+				
 			}
 
 			else if( $(this).hasClass('active') && $(this).hasClass('about-btn') ) {
 				
 				$('.navbar-header button.active').removeClass('active');
-				$('#about').addClass('active');
+				$('#about-btn').addClass('active');
 				console.log("about");
 			}
 
 			else if( $(this).hasClass('active') && $(this).hasClass('contact-btn') ) {
 				
 				$('.navbar-header button.active').removeClass('active');
-				$('#contact').addClass('active');
+				$('#contact-btn').addClass('active');
 				console.log("contact");
 				return false;
 			}
-			
+
 			else{
-				
 			}
+
 		});
-	}
-	$('.navbar-header button').click(function() {
-		activeNav();
-	});
-	
-//	Nav Buttons Add/Remove Active Class
-//	$('.navbar-header button').click(function() {
-//
-//    $('.navbar-header button.active').removeClass('active');
-//    $(this).addClass('active');
-//
-//	});
-//	
-	//	Indicators Add/Remove Active Class
-	$('.carousel-indicators li').click(function() {
-		activeNav();
-	});
-	
-		//	Control Left Add/Remove Active Class
-	$('.left .carousel-control').click(function() {
-		activeNav();
-	});
-	
-		//	Control Right Add/Remove Active Class
-	$('.right .carousel-control').click(function() {
-		activeNav();
-	});
-	
-	
-  $(document).scroll(function() {
-    var y = $(this).scrollTop();
-    if (y > 200) {
-      $(".carousel-indicators").fadeIn(200);
-    } else {
-      $(".carousel-indicators").fadeOut(200);
-    }
 	});
 	
 });
+	
